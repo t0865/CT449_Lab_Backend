ba8dcca303a2a18a03ee223e2540adc22cc106d4
Thanh Tuyen <tuyenb2113324@student.ctu.edu.vn>
2025-02-24 22:17:28 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route"); 
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application."});
+    res.json({ message: "Welcome to contact book application." });
+});
+
+// Middleware xử lý khi không tìm thấy route
+app.use((req, res, next) => {
+    next(new ApiError(404, "Resource not found"));
+});
+
+// Middleware xử lý lỗi chung
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
+
