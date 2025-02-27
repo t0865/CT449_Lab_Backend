ba830a5a2c9c1f44fbbfcccfd6d12f6f91a42c6a
Thanh Tuyen <tuyenb2113324@student.ctu.edu.vn>
2025-02-27 13:32:41 +0700

    Cai dat casc handler truy xuat CSDL

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const contacts = require("../controllers/contact.controller");
+const express = require('express')
+const contacts = require('../controllers/contact.controller.js')
 
-const router = express.Router();
+const router = express.Router()
 
 router.route("/")
     .get(contacts.findAll)
     .post(contacts.create)
-    .delete(contacts.deleteAll);
+    .delete(contacts.deleteAll)
 
 router.route("/favorite")
-    .get(contacts.findAllFavorite);
+    .get(contacts.findAllFavorite)
 
 router.route("/:id")
     .get(contacts.findOne)
     .put(contacts.update)
-    .delete(contacts.delete);
+    .delete(contacts.delete)
 
-module.exports = router;
+module.exports = router 
